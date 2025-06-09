3d41c5860af1ec50ea8761257d1bb9a863ee82b6
死亡二极管 <xfy13066344092@outlook.com>
2025-06-09 17:01:00 +0800

    提交代码

--- src/main/java/com/doubleC/demo/Banker.java
+++ src/main/java/com/doubleC/demo/Banker.java
@@ -11,11 +11,18 @@ class Process {
     private int[] need;
     private boolean finished;
 
-    //店中店构造函数，这段可以直接不看
+    //店中店构造函数，这段可以直接不看,唯一需要注意的是计算need的值
     public Process(String pID, int[] total, int[] allocation) {
         this.pID = pID;
         this.total = total;
         this.allocation = allocation;
+
+        this.need = new int[total.length];
+        for (int i =0; i < total.length ; i++){
+            need[i] = total[i] - allocation[i];
+        }
+
+        this.setFinished(false);
     }
 
     public String getpID() {
@@ -62,6 +69,7 @@ class Process {
     //进程分配检测函数
 
     public boolean canBeSatisfied(int[] resources){
+
         for (int i =0;i < resources.length ; i++){
             if (this.need[i] > resources[i]){
                 return false;
@@ -116,7 +124,7 @@ public class Banker {
 
                     //模拟资源释放
                     for (int i = 0; i < work.length ; i++){
-                        work[i] += p.getTotal()[i];
+                        work[i] += p.getAllocation()[i];
                     }
                     p.setFinished(true);
                     safe = true;
@@ -133,6 +141,27 @@ public class Banker {
         System.out.println("系统顺利运行! 安全队列为 : ");
         for (Process p: secureProcess){
             System.out.print(p.getpID() + " ");
+
         }
+    }
+
+
+    public static void main(String[] args) {
+        List<Process> processes = new ArrayList<>();
+        Process p1 = new Process("P1",new int[]{7,5,3},new int[]{0,1,0});
+        Process p2 = new Process("P2",new int[]{3,2,2},new int[]{2,0,0});
+        Process p3 = new Process("P3",new int[]{9,0,2},new int[]{3,0,2});
+        Process p4 = new Process("P4",new int[]{2,2,2},new int[]{2,1,1});
+        Process p5 = new Process("P5",new int[]{4,3,3},new int[]{0,0,2});
+        processes.add(p1);
+        processes.add(p2);
+        processes.add(p3);
+        processes.add(p4);
+        processes.add(p5);
+
+        int[] available = {3,3,2};
+        Banker banker = new Banker(available,processes);
+
+        banker.isSafe();
     }
 }
